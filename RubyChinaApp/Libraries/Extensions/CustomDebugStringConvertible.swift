623e1c72b5464e8af92270394b0e9102cf7f0118
jasl <jasl9187@hotmail.com>
2015-12-14 04:32:36 +0800

    refactor

--- RubyChinaApp/Libraries/Extensions/CustomDebugStringConvertible.swift
+++ RubyChinaApp/Libraries/Extensions/CustomDebugStringConvertible.swift
@@ -7,6 +7,52 @@ import Foundation
 
 extension CustomDebugStringConvertible {
     var debugDescription: String {
-        return debugDescriptionFor(self)
+        let mirror = Mirror(reflecting: self)
+
+        var children = [(label: String?, value: Any)]()
+        let mirrorChildrenCollection = AnyRandomAccessCollection(mirror.children)!
+        children += mirrorChildrenCollection
+
+        var currentMirror = mirror
+        while let superclassChildren = currentMirror.superclassMirror()?.children {
+            let randomCollection = AnyRandomAccessCollection(superclassChildren)!
+            children += randomCollection
+            currentMirror = currentMirror.superclassMirror()!
+        }
+
+        var filteredChildren = [(label: String?, value: Any)]()
+        for (optionalPropertyName, value) in children {
+            if !optionalPropertyName!.containsString("notMapped_") {
+                filteredChildren += [(optionalPropertyName, value)]
+            }
+        }
+
+        var index = 0
+
+        var str = "<\(String(mirror.subjectType))"
+
+        for (optionalPropertyName, value) in filteredChildren {
+            let propertyName = optionalPropertyName!
+
+            str += " \(propertyName)="
+
+            if let propertyValue = value as? CustomDebugStringConvertible {
+                str += propertyValue.debugDescription
+            } else {
+                let propertyValue = String(value)
+
+                if propertyValue == "nil" {
+                    str += "nil"
+                } else {
+                    str += propertyValue
+                }
+            }
+
+            index++
+        }
+
+        str += ">"
+
+        return str
     }
 }

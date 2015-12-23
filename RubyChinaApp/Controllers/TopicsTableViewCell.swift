da1c28d431728d3f4d1bfd2d6bf8a700eb602176
jasl <jasl9187@hotmail.com>
2015-12-23 21:05:27 +0800

    can not resolve "Could not insert new outlet connection"

---
+++ RubyChinaApp/Controllers/TopicsTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  TopicsTableViewCell.swift
+//  RubyChinaApp
+//
+//  Created by 姜军 on 12/23/15.
+//  Copyright © 2015 RubyChina. All rights reserved.
+//
+
+import UIKit
+
+class TopicsTableViewCell: UITableViewCell {
+
+    // MARK: Properties
+    var foo: String
+    
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
